7c7db0953252d74376492124f0c65622a53d80a8
FreedomMistrall <mistrall911@gmail.com>
2018-12-21 18:52:14 +0200

    func small_string

--- index.php
+++ index.php
@@ -6,4 +6,28 @@ index_admin();
 
 include 'templates/header.php';
 
+
+
+
+$str = 'Это помогает избежать добавления случайных символов пробела или перевода строки после закрывающего тега PHP.';
+$limit = 50;
+
+function small_string($str,$limit) {                         
+    $array = explode(' ', $str);                  
+    $string = '';                               
+    foreach ($array as $word) {
+        $string = $string . ' ' . $word;           
+        if (strlen($string) >= $limit) {          
+            $smallString = $string . ' ...';        
+            break;                              
+        }                                       
+        else                                    
+            $smallString = $string;               
+        }                                       
+    return $smallString;                         
+}
+$result = small_string($str,$limit);
+print_r($result);
+
+
 ?>
